1ed878cc65ea596846394de801ecc6dbb2e525a0
Riccardo Schirone <riccardo.schirone@trailofbits.com>
2025-01-17 23:18:46 +0000

    common/protos: add orchestrator-specific messages

---
+++ common/src/buttercup/common/datastructures/orchestrator_pb2.pyi
@@ -0,0 +1,57 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class SourceDetail(_message.Message):
+    __slots__ = ["sha256", "source_type", "url"]
+    class SourceType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+    SHA256_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_TYPE_DIFF: SourceDetail.SourceType
+    SOURCE_TYPE_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_TYPE_FUZZ_TOOLING: SourceDetail.SourceType
+    SOURCE_TYPE_REPO: SourceDetail.SourceType
+    URL_FIELD_NUMBER: _ClassVar[int]
+    sha256: str
+    source_type: SourceDetail.SourceType
+    url: str
+    def __init__(self, sha256: _Optional[str] = ..., source_type: _Optional[_Union[SourceDetail.SourceType, str]] = ..., url: _Optional[str] = ...) -> None: ...
+
+class Task(_message.Message):
+    __slots__ = ["deadline", "message_id", "message_time", "sources", "task_id", "task_status", "task_type"]
+    class TaskStatus(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+    class TaskType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+    DEADLINE_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_ID_FIELD_NUMBER: _ClassVar[int]
+    MESSAGE_TIME_FIELD_NUMBER: _ClassVar[int]
+    SOURCES_FIELD_NUMBER: _ClassVar[int]
+    TASK_ID_FIELD_NUMBER: _ClassVar[int]
+    TASK_STATUS_CANCELLED: Task.TaskStatus
+    TASK_STATUS_FAILED: Task.TaskStatus
+    TASK_STATUS_FIELD_NUMBER: _ClassVar[int]
+    TASK_STATUS_PENDING: Task.TaskStatus
+    TASK_STATUS_RUNNING: Task.TaskStatus
+    TASK_STATUS_SUCCEEDED: Task.TaskStatus
+    TASK_TYPE_DELTA: Task.TaskType
+    TASK_TYPE_FIELD_NUMBER: _ClassVar[int]
+    TASK_TYPE_FULL: Task.TaskType
+    deadline: int
+    message_id: str
+    message_time: int
+    sources: _containers.RepeatedCompositeFieldContainer[SourceDetail]
+    task_id: str
+    task_status: Task.TaskStatus
+    task_type: Task.TaskType
+    def __init__(self, message_id: _Optional[str] = ..., message_time: _Optional[int] = ..., task_id: _Optional[str] = ..., task_type: _Optional[_Union[Task.TaskType, str]] = ..., sources: _Optional[_Iterable[_Union[SourceDetail, _Mapping]]] = ..., deadline: _Optional[int] = ..., task_status: _Optional[_Union[Task.TaskStatus, str]] = ...) -> None: ...
+
+class TaskDownload(_message.Message):
+    __slots__ = ["task_id"]
+    TASK_ID_FIELD_NUMBER: _ClassVar[int]
+    task_id: str
+    def __init__(self, task_id: _Optional[str] = ...) -> None: ...
