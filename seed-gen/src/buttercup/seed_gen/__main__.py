6625ff4952455873313858afcff83ac862145a91
Ronald Eytchison <ronald.eytchison@trailofbits.com>
2025-01-21 22:47:12 +0000

    Match build system and file layout of other components

--- seed-gen/src/buttercup/seed_gen/__main__.py
+++ seed-gen/src/buttercup/seed_gen/__main__.py
@@ -1,6 +1,6 @@
 """The `python -m seed-gen` entrypoint."""
 
 if __name__ == "__main__":  # pragma: no cover
-    from seed_gen._cli import main
+    from buttercup.seed_gen._cli import main
 
     main()
--- seed-gen/test/test_init.py
+++
@@ -1,9 +0,0 @@
-"""Initial testing module."""
-
-import seed_gen
-
-
-def test_version() -> None:
-    version = getattr(seed_gen, "__version__", None)
-    assert version is not None
-    assert isinstance(version, str)
