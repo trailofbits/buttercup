5aa393a1568278831b1e6dbcee25987c44314e20
Ronald Eytchison <ronald.eytchison@trailofbits.com>
2025-01-24 17:09:50 +0000

    Copy target from fuzzer target list into seedgen dir

--- seed-gen/src/buttercup/seed_gen/_cli.py
+++ seed-gen/src/buttercup/seed_gen/_cli.py
@@ -1,25 +1,24 @@
 """The `seed-gen` entrypoint."""
 
 import argparse
-import logging
 import os
 import random
 import tempfile
 import time
+from pathlib import Path
 
 from redis import Redis
 
 from buttercup.common import utils
 from buttercup.common.datastructures.fuzzer_msg_pb2 import WeightedTarget
+from buttercup.common.logger import setup_logging
 from buttercup.common.queues import (
     NormalQueue,
     QueueNames,
     SerializationDeserializationQueue,
 )
-from buttercup.seed_gen.logger import logger_configurer
 
-logger_configurer(os.getenv("LOG_LEVEL", "INFO").upper())
-logger = logging.getLogger(__name__)
+logger = setup_logging(__name__, os.getenv("LOG_LEVEL", "INFO").upper())
 
 
 def main() -> None:
@@ -32,6 +31,12 @@ def main() -> None:
     parser_server.add_argument("--wdir", required=True, help="Working directory")
     parser_server.add_argument(
         "--sleep", required=False, default=1, type=int, help="Sleep between runs (seconds)"
+    )
+    parser_server.add_argument(
+        "--local_crs_scratch",
+        required=False,
+        type=Path,
+        help="Local path to the /crs_scratch volume directory (for running seed-gen locally)",
     )
     args = parser.parse_args()
     if args.command == "server":
@@ -47,17 +52,22 @@ def command_server(args: argparse.Namespace) -> None:
         weighted_items: list[WeightedTarget] = list(iter(q))
 
         if len(weighted_items) > 0:
-            with tempfile.TemporaryDirectory(prefix=args.wdir) as td:
+            with tempfile.TemporaryDirectory(dir=args.wdir) as td:
                 chc = random.choices(
                     [it for it in weighted_items],
                     weights=[it.weight for it in weighted_items],
                     k=1,
                 )[0]
                 logger.info(
-                    "Starting run on challenge %s harness %s",
+                    "Starting run on challenge %s at path %s",
                     chc.target.package_name,
-                    chc.harness_path,
+                    chc.target.output_ossfuzz_path,
                 )
-                build_dir = os.path.dirname(chc.harness_path)
-                utils.copyanything(build_dir, os.path.join(td, os.path.basename(build_dir)))
+                target_dir = Path(chc.target.output_ossfuzz_path)
+                if args.local_crs_scratch:
+                    target_dir = args.local_crs_scratch / target_dir.relative_to("/crs_scratch")
+                    logger.info("Using local dir in local crs_scratch: %s", target_dir)
+                dest_dir = Path(td) / target_dir.name
+                utils.copyanything(target_dir, dest_dir)
+                logger.info("Copied target to %s", dest_dir)
         time.sleep(args.sleep)
