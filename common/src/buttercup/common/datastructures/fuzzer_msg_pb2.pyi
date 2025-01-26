92afd439d484954a7db470c5427f807b38f18e7c
2over12 <ian.smith@trailofbits.com>
2025-01-26 18:54:05 +0000

    add source dir

--- common/src/buttercup/common/datastructures/fuzzer_msg_pb2.pyi
+++ common/src/buttercup/common/datastructures/fuzzer_msg_pb2.pyi
@@ -5,28 +5,32 @@ from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Opti
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class BuildOutput(_message.Message):
-    __slots__ = ["engine", "output_ossfuzz_path", "package_name", "sanitizer"]
+    __slots__ = ["engine", "output_ossfuzz_path", "package_name", "sanitizer", "source_path"]
     ENGINE_FIELD_NUMBER: _ClassVar[int]
     OUTPUT_OSSFUZZ_PATH_FIELD_NUMBER: _ClassVar[int]
     PACKAGE_NAME_FIELD_NUMBER: _ClassVar[int]
     SANITIZER_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_PATH_FIELD_NUMBER: _ClassVar[int]
     engine: str
     output_ossfuzz_path: str
     package_name: str
     sanitizer: str
-    def __init__(self, package_name: _Optional[str] = ..., engine: _Optional[str] = ..., sanitizer: _Optional[str] = ..., output_ossfuzz_path: _Optional[str] = ...) -> None: ...
+    source_path: str
+    def __init__(self, package_name: _Optional[str] = ..., engine: _Optional[str] = ..., sanitizer: _Optional[str] = ..., output_ossfuzz_path: _Optional[str] = ..., source_path: _Optional[str] = ...) -> None: ...
 
 class BuildRequest(_message.Message):
-    __slots__ = ["engine", "ossfuzz", "package_name", "sanitizer"]
+    __slots__ = ["engine", "ossfuzz", "package_name", "sanitizer", "source_path"]
     ENGINE_FIELD_NUMBER: _ClassVar[int]
     OSSFUZZ_FIELD_NUMBER: _ClassVar[int]
     PACKAGE_NAME_FIELD_NUMBER: _ClassVar[int]
     SANITIZER_FIELD_NUMBER: _ClassVar[int]
+    SOURCE_PATH_FIELD_NUMBER: _ClassVar[int]
     engine: str
     ossfuzz: str
     package_name: str
     sanitizer: str
-    def __init__(self, package_name: _Optional[str] = ..., engine: _Optional[str] = ..., sanitizer: _Optional[str] = ..., ossfuzz: _Optional[str] = ...) -> None: ...
+    source_path: str
+    def __init__(self, package_name: _Optional[str] = ..., engine: _Optional[str] = ..., sanitizer: _Optional[str] = ..., ossfuzz: _Optional[str] = ..., source_path: _Optional[str] = ...) -> None: ...
 
 class WeightedTarget(_message.Message):
     __slots__ = ["harness_path", "target", "weight"]
