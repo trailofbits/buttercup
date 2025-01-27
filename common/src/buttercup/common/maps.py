fb58789bb5ad7f6592b07b7f9bd58d9551101d69
2over12 <ian.smith@trailofbits.com>
2025-01-27 13:37:41 +0000

    type bound

--- common/src/buttercup/common/maps.py
+++ common/src/buttercup/common/maps.py
@@ -2,9 +2,9 @@ from typing import Generic, TypeVar, Type, Iterator
 from buttercup.common.datastructures.fuzzer_msg_pb2 import WeightedTarget
 from redis import Redis
 from bson.json_util import dumps, CANONICAL_JSON_OPTIONS
+from google.protobuf.message import Message
 
-
-MsgType = TypeVar("MsgType")
+MsgType = TypeVar("MsgType", bound=Message)
 MSG_FIELD_NAME = b"msg"
 
 
