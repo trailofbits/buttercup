0c3c88a57b61610925ea3ab9f38ec53bd751411b
2over12 <ian.smith@trailofbits.com>
2025-01-27 08:48:32 -0500

    Separate corpuses and Hash Inputs (#34)
    * corpus object
    
    * lint fix
    
    * own file hash
    
    * format
    
    * revert testing timeout

--- fuzzer/src/buttercup/fuzzing_infra/fuzzer_bot.py
+++ fuzzer/src/buttercup/fuzzing_infra/fuzzer_bot.py
@@ -1,15 +1,13 @@
 import argparse
-import distutils.dir_util
 from buttercup.fuzzing_infra.runner import Runner, Conf, FuzzConfiguration
 import time
 import os
 from buttercup.common.datastructures.fuzzer_msg_pb2 import WeightedTarget
 from buttercup.common.maps import FuzzerMap
-from buttercup.common.constants import CORPUS_DIR_NAME
 from buttercup.common import utils
+from buttercup.common.corpus import Corpus
 import random
 import tempfile
-import distutils
 from buttercup.common.logger import setup_logging
 from redis import Redis
 
@@ -46,11 +44,11 @@ def main():
                 logger.info(f"Running fuzzer for {chc.target.engine} | {chc.target.sanitizer} | {chc.harness_path}")
 
                 build_dir = os.path.dirname(chc.harness_path)
-                corpdir = os.path.join(build_dir, CORPUS_DIR_NAME)
-                os.makedirs(corpdir, exist_ok=True)
+                corp = Corpus(chc.harness_path)
+
                 utils.copyanything(build_dir, os.path.join(td, os.path.basename(build_dir)))
                 copied_build_dir = os.path.join(td, os.path.basename(build_dir))
-                copied_corp_dir = os.path.join(copied_build_dir, CORPUS_DIR_NAME)
+                copied_corp_dir = os.path.join(copied_build_dir, corp.basename())
                 tgtbuild = chc.target
                 fuzz_conf = FuzzConfiguration(
                     copied_corp_dir,
@@ -60,7 +58,7 @@ def main():
                 )
                 logger.info(f"Starting fuzzer {chc.target.engine} | {chc.target.sanitizer} | {chc.harness_path}")
                 runner.run_fuzzer(fuzz_conf)
-                distutils.dir_util.copy_tree(copied_corp_dir, corpdir)
+                corp.copy_corpus(copied_corp_dir)
                 logger.info(f"Fuzzer finished for {chc.target.engine} | {chc.target.sanitizer} | {chc.harness_path}")
 
         logger.info(f"Sleeping for {seconds_sleep} seconds")
