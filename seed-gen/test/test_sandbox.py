633cb7c7c1778a3dd317447a24cbf3e8405104a9
Ronald Eytchison <ronald.eytchison@trailofbits.com>
2025-02-05 19:28:08 +0000

    Fix test case

--- seed-gen/test/test_sandbox.py
+++ seed-gen/test/test_sandbox.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from pathlib import Path
 
 from buttercup.seed_gen.sandbox.sandbox import sandbox_exec_funcs
@@ -9,10 +10,17 @@ expected_seeds = [
 ]
 
 
+def read_seeds(outdir: Path) -> list[bytes]:
+    return [file.read_bytes() for file in outdir.iterdir()]
+
+
 def test_sandbox_exec_funcs():
     file_path = os.path.abspath(__file__)
     test_file = Path(file_path).parent / "data/example_seed_funcs.py"
     expected_seeds.sort()
-    seeds = sandbox_exec_funcs(test_file.read_text())
-    seeds.sort()
-    assert seeds == expected_seeds
+    with tempfile.TemporaryDirectory() as outdir_str:
+        outdir = Path(outdir_str)
+        sandbox_exec_funcs(test_file.read_text(), outdir)
+        seeds = read_seeds(outdir)
+        seeds.sort()
+        assert seeds == expected_seeds
