2b7c6b9fae6774a4cfa7cfd1084948245339f9c5
Ronald Eytchison <ronald.eytchison@trailofbits.com>
2025-02-05 19:20:23 +0000

    Move SeedGenBot to new file

--- seed-gen/src/buttercup/seed_gen/_cli.py
+++ seed-gen/src/buttercup/seed_gen/_cli.py
@@ -2,56 +2,15 @@
 
 import argparse
 import os
-import tempfile
 from pathlib import Path
 
 from redis import Redis
 
-from buttercup.common import utils
-from buttercup.common.corpus import Corpus
-from buttercup.common.datastructures.msg_pb2 import BuildOutput, WeightedHarness
-from buttercup.common.default_task_loop import TaskLoop
 from buttercup.common.logger import setup_logging
-from buttercup.common.maps import BUILD_TYPES
+from buttercup.seed_gen.seed_gen_bot import SeedGenBot
 from buttercup.seed_gen.tasks import Task, do_seed_explore, do_seed_init, do_vuln_discovery
 
 logger = setup_logging(__name__, os.getenv("LOG_LEVEL", "INFO").upper())
-
-
-class SeedGenBot(TaskLoop):
-    def __init__(self, redis: Redis, timer_seconds: int, wdir: str):
-        self.wdir = wdir
-        super().__init__(redis, timer_seconds)
-
-    def required_builds(self) -> list[BUILD_TYPES]:
-        return [BUILD_TYPES.FUZZER]
-
-    def run_task(self, task: WeightedHarness, builds: dict[BUILD_TYPES, BuildOutput]):
-        with tempfile.TemporaryDirectory(dir=self.wdir, prefix="seedgen-") as temp_dir_str:
-            logger.info(
-                f"Running seed-gen for {task.harness_name} | {task.package_name} | {task.task_id}"
-            )
-            temp_dir = Path(temp_dir_str)
-            logger.debug(f"Temp dir: {temp_dir}")
-            out_dir = temp_dir / "seedgen-out"
-            out_dir.mkdir()
-
-            corp = Corpus(self.wdir, task.task_id, task.harness_name)
-
-            build = builds[BUILD_TYPES.FUZZER]
-            logger.info(f"Build dir: {build.output_ossfuzz_path}")
-            output_ossfuzz_path = Path(build.output_ossfuzz_path)
-            build_dir = output_ossfuzz_path / "build/out" / build.package_name
-            copied_build_dir = temp_dir / build_dir.name
-            utils.copyanything(build_dir, copied_build_dir)
-
-            do_seed_init(build.package_name, out_dir)
-            num_files = sum(1 for _ in out_dir.iterdir())
-            logger.info("Copying %d files to corpus %s", num_files, corp.corpus_dir)
-            corp.copy_corpus(out_dir)
-            logger.info(
-                f"Seed-gen finished for {task.harness_name} | {task.package_name} | {task.task_id}"
-            )
 
 
 def command_server(args: argparse.Namespace) -> None:
