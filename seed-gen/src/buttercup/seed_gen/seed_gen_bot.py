a0dcee256198be88a8cb709b3b24f750f9f05b60
Ronald Eytchison <ronald.eytchison@trailofbits.com>
2025-02-06 17:08:59 +0000

    Randomly select seed-gen task in SeedGenBot

--- seed-gen/src/buttercup/seed_gen/seed_gen_bot.py
+++ seed-gen/src/buttercup/seed_gen/seed_gen_bot.py
@@ -1,4 +1,5 @@
 import os
+import random
 import tempfile
 from pathlib import Path
 
@@ -9,7 +10,7 @@ from buttercup.common.datastructures.msg_pb2 import BuildOutput, WeightedHarness
 from buttercup.common.default_task_loop import TaskLoop
 from buttercup.common.logger import setup_logging
 from buttercup.common.maps import BUILD_TYPES
-from buttercup.seed_gen.tasks import do_seed_init
+from buttercup.seed_gen.tasks import Task, do_seed_init, do_vuln_discovery
 
 logger = setup_logging(__name__, os.getenv("LOG_LEVEL", "INFO").upper())
 
@@ -33,8 +34,15 @@ class SeedGenBot(TaskLoop):
             out_dir.mkdir()
 
             corp = Corpus(self.wdir, task.task_id, task.harness_name)
+            task_choice = random.choices([Task.SEED_INIT, Task.VULN_DISCOVERY], k=1)[0]
+            logger.info(f"Running seed-gen task: {task_choice.value}")
+            if task_choice == Task.SEED_INIT:
+                do_seed_init(task.package_name, out_dir)
+            elif task_choice == Task.VULN_DISCOVERY:
+                do_vuln_discovery(task.package_name, out_dir)
+            else:
+                raise ValueError(f"Unexpected task: {task_choice}")
 
-            do_seed_init(task.package_name, out_dir)
             num_files = sum(1 for _ in out_dir.iterdir())
             logger.info("Copying %d files to corpus %s", num_files, corp.corpus_dir)
             corp.copy_corpus(out_dir)
