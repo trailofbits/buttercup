ff5e3d01d675a7203a95c1910af2facb1e5bdc56
Henrik Brodin <90325907+hbrodin@users.noreply.github.com>
2025-03-25 13:38:32 +0100

    Update competition api to v1.1-rc4 (#240)

--- orchestrator/scripts/task_crs.sh
+++ orchestrator/scripts/task_crs.sh
@@ -2,7 +2,7 @@
 curl -X 'POST' 'http://127.0.0.1:31323/webhook/trigger_task' -H 'Content-Type: application/json' -d '{
     "challenge_repo_url": "git@github.com:aixcc-finals/example-libpng.git",
     "challenge_repo_base_ref": "0cc367aaeaac3f888f255cee5d394968996f736e",
-    "challenge_repo_head_ref": "2c894c66108f0724331a9e5b4826e351bf2d094b",
+    "challenge_repo_head_ref": "92407e9d102e36f5538dea21ca87b0dc27817126",
     "fuzz_tooling_url": "git@github.com:aixcc-finals/oss-fuzz-aixcc.git",
     "fuzz_tooling_ref": "d5fbd68fca66e6fa4f05899170d24e572b01853d",
     "fuzz_tooling_project_name": "libpng",
