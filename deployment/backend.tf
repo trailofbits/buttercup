379e3efa7ca165a77aa5b656ff68cf26f0fe53d0
Riccardo Schirone <562321+ret2libc@users.noreply.github.com>
2025-03-29 15:18:02 +0100

    deployment: fix disabling competition-api (#304)

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateserviceact"
+    storage_account_name = "tfstateserviceact" # "tfstateserviceactprod"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
