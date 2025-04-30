0140af533a099734a0b8565ab36098ae8b2a5526
Riccardo Schirone <562321+ret2libc@users.noreply.github.com>
2025-04-30 13:53:42 +0200

    Improve logs collection (#500)

--- common/src/buttercup/common/logger.py
+++ common/src/buttercup/common/logger.py
@@ -16,13 +16,19 @@ def setup_package_logger(logger_name: str, log_level: str = "info") -> logging.L
             for handler in root.handlers:
                 root.removeHandler(handler)
 
+        persistent_log_dir = os.getenv("PERSISTENT_LOG_DIR", None)
+
+        handlers = [
+            logging.StreamHandler(),
+            logging.FileHandler(os.path.join(tempfile.gettempdir(), f"{logger_name}.log")),
+        ]
+        if persistent_log_dir:
+            handlers.append(logging.FileHandler(os.path.join(persistent_log_dir, f"{logger_name}.log")))
+
         # Configure root logger
         logging.basicConfig(
             format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            handlers=[
-                logging.StreamHandler(),
-                logging.FileHandler(os.path.join(tempfile.gettempdir(), f"{logger_name}.log")),
-            ],
+            handlers=handlers,
         )
 
         _package_logger = logging.getLogger(PACKAGE_LOGGER_NAME)
