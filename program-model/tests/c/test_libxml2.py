2d32b44c51e63ea7a1c51812a303d7cebeaebec9
Evan Downing <2077950+evandowning@users.noreply.github.com>
2025-05-19 21:21:08 -0400

    Update libxml2 after unscored-2 (#639)

--- program-model/tests/c/test_libxml2.py
+++ program-model/tests/c/test_libxml2.py
@@ -2,10 +2,17 @@ import pytest
 
 from buttercup.common.challenge_task import ChallengeTask
 from ..common import (
+    common_test_get_callers,
     common_test_get_callees,
     common_test_get_functions,
+    common_test_get_type_definitions,
+    common_test_get_type_usages,
     TestFunctionInfo,
     TestCalleeInfo,
+    TestCallerInfo,
+    TestTypeDefinitionInfo,
+    TestTypeUsageInfo,
+    TypeDefinitionType,
 )
 
 
@@ -40,6 +47,47 @@ def test_get_functions(
 
 
 @pytest.mark.parametrize(
+    "function_name,file_path,line_number,fuzzy,expected_callers,num_callers",
+    [
+        (
+            "xmlParse3986Host",
+            "/src/libxml2/uri.c",
+            None,
+            False,
+            [
+                TestCallerInfo(
+                    name="xmlParse3986Authority",
+                    file_path="/src/libxml2/uri.c",
+                    start_line=553,
+                ),
+            ],
+            1,
+        ),
+    ],
+)
+@pytest.mark.integration
+def test_get_callers(
+    libxml2_oss_fuzz_task: ChallengeTask,
+    function_name,
+    file_path,
+    line_number,
+    fuzzy,
+    expected_callers,
+    num_callers,
+):
+    """Test that we can get function callers."""
+    common_test_get_callers(
+        libxml2_oss_fuzz_task,
+        function_name,
+        file_path,
+        line_number,
+        fuzzy,
+        expected_callers,
+        num_callers,
+    )
+
+
+@pytest.mark.parametrize(
     "function_name,file_path,line_number,fuzzy,expected_callees,num_callees",
     [
         (
@@ -51,7 +99,7 @@ def test_get_functions(
                 TestCalleeInfo(
                     name="xmlURIUnescapeString",
                     file_path="/src/libxml2/uri.c",
-                    start_line=1611,
+                    start_line=1619,
                 ),
                 TestCalleeInfo(
                     name="xmlParse3986DecOctet",
@@ -82,4 +130,113 @@ def test_get_callees(
         fuzzy,
         expected_callees,
         num_callees,
+    )
+
+
+@pytest.mark.parametrize(
+    "type_name,file_path,fuzzy,type_definition_info",
+    [
+        (
+            "xmlURI",
+            None,
+            False,
+            TestTypeDefinitionInfo(
+                name="xmlURI",
+                type=TypeDefinitionType.TYPEDEF,
+                definition="typedef struct _xmlURI xmlURI;",
+                definition_line=32,
+                file_path="/src/libxml2/include/libxml/uri.h",
+            ),
+        ),
+        (
+            "xmlURIPtr",
+            None,
+            False,
+            TestTypeDefinitionInfo(
+                name="xmlURIPtr",
+                type=TypeDefinitionType.TYPEDEF,
+                definition="typedef xmlURI *xmlURIPtr;",
+                definition_line=33,
+                file_path="/src/libxml2/include/libxml/uri.h",
+            ),
+        ),
+        (
+            "_xmlURI",
+            None,
+            False,
+            TestTypeDefinitionInfo(
+                name="_xmlURI",
+                type=TypeDefinitionType.STRUCT,
+                definition="""struct _xmlURI {
+    char *scheme;	/* the URI scheme */
+    char *opaque;	/* opaque part */
+    char *authority;	/* the authority part */
+    char *server;	/* the server part */
+    char *user;		/* the user part */
+    int port;		/* the port number */
+    char *path;		/* the path string */
+    char *query;	/* the query string (deprecated - use with caution) */
+    char *fragment;	/* the fragment identifier */
+    int  cleanup;	/* parsing potentially unclean URI */
+    char *query_raw;	/* the query string (as it appears in the URI) */
+}""",
+                definition_line=34,
+                file_path="/src/libxml2/include/libxml/uri.h",
+            ),
+        ),
+    ],
+)
+@pytest.mark.integration
+def test_get_type_definitions(
+    libxml2_oss_fuzz_task: ChallengeTask,
+    type_name,
+    file_path,
+    fuzzy,
+    type_definition_info,
+):
+    """Test that we can get type defs"""
+    common_test_get_type_definitions(
+        libxml2_oss_fuzz_task,
+        type_name,
+        file_path,
+        fuzzy,
+        type_definition_info,
+    )
+
+
+@pytest.mark.parametrize(
+    "type_name,file_path,fuzzy,type_usage_infos,num_type_usages",
+    [
+        (
+            "xmlURIPtr",
+            None,
+            False,
+            [
+                TestTypeUsageInfo(
+                    file_path="/src/libxml2/uri.c",
+                    line_number=1085,
+                ),
+            ],
+            6,
+        ),
+    ],
+)
+@pytest.mark.skip(reason="Skipping because codequery doesn't think xmlURIPtr is a type")
+@pytest.mark.integration
+def test_get_type_usages(
+    libxml2_oss_fuzz_task: ChallengeTask,
+    type_name,
+    file_path,
+    fuzzy,
+    type_usage_infos,
+    num_type_usages,
+):
+    """Test that we can get function callees from zookeeper"""
+    common_test_get_type_usages(
+        libxml2_oss_fuzz_task,
+        type_name,
+        file_path,
+        fuzzy,
+        type_usage_infos,
+        num_type_usages,
     )
