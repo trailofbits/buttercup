f2cb357ad7798f546e9b9670c4edd3f914265cc0
Riccardo Schirone <562321+ret2libc@users.noreply.github.com>
2025-06-25 15:27:04 +0200

    Include backend changes and Bump v0.4.3 (#937)

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateserviceact" # "tfstateserviceactprod"
+    resource_group_name  = "example-tfstate-rg"
+    storage_account_name = "tffinalacc"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
