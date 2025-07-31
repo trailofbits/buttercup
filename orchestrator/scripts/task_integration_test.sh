a08cbf1050eea363cd312c7612d5e855bc703d58
Riccardo Schirone <562321+ret2libc@users.noreply.github.com>
2025-07-31 15:34:28 +0200

    Make CRS more flexible (#202)
    * Fix integration-test run
    
    * use the proper oss-fuzz-aixcc commit to run integration-test
    * determine OSS_FUZZ_CONTAINER_ORG at runtime
    * use `git diff` in the patcher to create the patch to fix a problem
      with patches affecting non-newline terminated files
    
    * fix parsing
    
    * fix program-model lint

--- orchestrator/scripts/task_integration_test.sh
+++ orchestrator/scripts/task_integration_test.sh
@@ -4,7 +4,7 @@ curl -X 'POST' 'http://127.0.0.1:31323/webhook/trigger_task' -H 'Content-Type: a
     "challenge_repo_base_ref": "4a714359c60858e3821bd478dc846de1d04dc977",
     "challenge_repo_head_ref": "challenges/integration-test-delta-01",
     "fuzz_tooling_url": "https://github.com/tob-challenges/oss-fuzz-aixcc",
-    "fuzz_tooling_ref": "ffa771deda2156995e80f76107f90dec14218b7b",
+    "fuzz_tooling_ref": "challenge-state/integration-test-delta-01",
     "fuzz_tooling_project_name": "integration-test",
     "duration": 1800
 }'
