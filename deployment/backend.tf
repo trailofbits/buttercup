b3bde6c34f102f6a8016f01ece586ba44199bb98
Riccardo Schirone <riccardo.schirone@trailofbits.com>
2025-09-08 10:45:35 +0200

    restore backend.tf

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "aixcc"
-    storage_account_name = "buttercuptf42cv"
+    resource_group_name  = "example-tfstate-rg"
+    storage_account_name = "tffinalacc"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
