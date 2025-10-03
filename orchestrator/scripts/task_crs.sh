033515ed249e36bae2a94c22182702bf5ab4b014
Riccardo Schirone <riccardo.schirone@trailofbits.com>
2025-10-03 11:30:27 +0200

    Use our modified oss-fuzz
    libpng has changed the default branch to libpng18, but that doesn't
    contain the oss-fuzz files. Let's use our own fork of oss-fuzz for now,
    until https://github.com/google/oss-fuzz/pull/14080 is ready.

--- orchestrator/scripts/task_crs.sh
+++ orchestrator/scripts/task_crs.sh
@@ -3,8 +3,8 @@ curl -X 'POST' 'http://127.0.0.1:31323/webhook/trigger_task' -H 'Content-Type: a
     "challenge_repo_url": "https://github.com/tob-challenges/example-libpng",
     "challenge_repo_base_ref": "5bf8da2d7953974e5dfbd778429c3affd461f51a",
     "challenge_repo_head_ref": "challenges/lp-delta-01",
-    "fuzz_tooling_url": "https://github.com/google/oss-fuzz",
-    "fuzz_tooling_ref": "master",
+    "fuzz_tooling_url": "https://github.com/trail-of-forks/oss-fuzz",
+    "fuzz_tooling_ref": "fix-libpng",
     "fuzz_tooling_project_name": "libpng",
     "duration": 1800
 }'
