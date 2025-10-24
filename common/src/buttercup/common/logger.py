094c203500dfb1592be7b82ac8d0e996c66f2504
Riccardo Schirone <riccardo.schirone@trailofbits.com>
2025-10-24 15:30:51 +0200

    lint 2

--- common/src/buttercup/common/logger.py
+++ common/src/buttercup/common/logger.py
@@ -6,7 +6,7 @@ try:
     if os.environ.get("OTEL_EXPORTER_OTLP_PROTOCOL") == "grpc":
         from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
     else:
-        from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
+        from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter  # type: ignore
 
     from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
     from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
